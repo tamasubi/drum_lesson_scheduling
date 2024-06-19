1d7be85457c4b7c5d092f8fbb283c11045ee9ee3
tamasubi <hidtamas@gmail.com>
2024-06-19 12:29:21 +0200

    register form add

--- students/views.py
+++ students/views.py
@@ -2,30 +2,44 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import studentCreationForm
 
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-        # Redirect to a success page.
-            return redirect('home')
-        else:
-        # Return an 'invalid login' error message.
-            messages.success(request, ("There Was An Error Logging In Try Again..."))
-            return redirect('login')
-    
-    else:
-        return render(request, 'authenticate/login.html', {})
-    
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
+			return redirect('login')	
 
+
+	else:
+		return render(request, 'authenticate/login.html', {})
 
 def logout_user(request):
-    logout(request)
-    messages.success(request, ("You were logged out"))
-    return redirect('home')
+	logout(request)
+	messages.success(request, ("You Were Logged Out!"))
+	return redirect('home')
+
 
 def register_user(request):
-    return render(request, 'authenticate/register_user.html',{})
+	if request.method == "POST":
+		form = studentCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Registration Successful!"))
+			return redirect('home')
+	else:
+		form = studentCreationForm()
+
+	return render(request, 'authenticate/register_user.html', {
+		'form':form,
+		})
