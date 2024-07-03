e56e399f8d55620563dba2f3c60d4a56a4a297ff
tamasubi <hidtamas@gmail.com>
2024-07-03 12:39:12 +0200

    dates_login, date kivalasztas userenkent

--- orak/views.py
+++ orak/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import drumlesson_option, student
 
 
 def dates(request):
     date_list = drumlesson_option.objects.all().exclude(student__isnull=False)
-    print(len(date_list))
-    return render(request, 'orak/dates.html', {'date_list':date_list})
+    if request.method == 'POST':
+        selected_date_id = request.POST.get('drumlesson_option')
+        if selected_date_id is not None:
+            try:
+                selected_date = drumlesson_option.objects.get(id=int(selected_date_id))
+                student = request.user
+                student.drumlesson = selected_date
+                student.save()
+                return redirect('home')
+            except ValueError:
+                print("Invalid date selected")
+                return redirect('dates')
+    else:
+        return render(request, 'orak/dates.html', {'date_list': date_list})
 
 
 def home(request):
